f3b2a0ae8a8505d41de88e8f5f7a8c53091b2219
Kevin <kutbass@163.com>
2025-08-31 23:35:39 +0800

    播放页面布局实现

---
+++ MyVideoPlayer-Client/playpage.cpp
@@ -0,0 +1,13 @@
+#include "playpage.h"
+#include "ui_playpage.h"
+
+PlayPage::PlayPage(QWidget *parent)
+    : QWidget(parent), ui(new Ui::PlayPage)
+{
+    ui->setupUi(this);
+}
+
+PlayPage::~PlayPage()
+{
+    delete ui;
+}
