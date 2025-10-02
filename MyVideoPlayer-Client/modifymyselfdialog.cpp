e25a54c0e371dbb8844687d6cec942091269fffb
Kevin <kutbass@163.com>
2025-10-02 14:36:51 +0800

    修改密码页面实现

--- MyVideoPlayer-Client/modifymyselfdialog.cpp
+++ MyVideoPlayer-Client/modifymyselfdialog.cpp
@@ -1,14 +1,39 @@
 #include "modifymyselfdialog.h"
 #include "ui_modifymyselfdialog.h"
+#include "util.h"
+#include "newpassworddialog.h"
 
 ModifyMyselfDialog::ModifyMyselfDialog(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::ModifyMyselfDialog)
+    : QDialog(parent), ui(new Ui::ModifyMyselfDialog)
 {
     ui->setupUi(this);
+    setWindowFlag(Qt::FramelessWindowHint);
+    ui->passwordWidget->hide();
+
+    connect(ui->submitBtn, &QPushButton::clicked, this, &ModifyMyselfDialog::onSubmitBtnClicked);
+    connect(ui->cancelBtn, &QPushButton::clicked, this, &ModifyMyselfDialog::onCancelBtnClicked);
+    connect(ui->passwordBtn, &QPushButton::clicked, this, &ModifyMyselfDialog::showPasswordDlg);
+    connect(ui->changePasswordBtn, &QPushButton::clicked, this, &ModifyMyselfDialog::showPasswordDlg);
 }
 
 ModifyMyselfDialog::~ModifyMyselfDialog()
 {
     delete ui;
+}
+
+void ModifyMyselfDialog::onSubmitBtnClicked()
+{
+    LOG() << "提交";
+}
+
+void ModifyMyselfDialog::onCancelBtnClicked()
+{
+    close();
+}
+
+void ModifyMyselfDialog::showPasswordDlg()
+{
+    NewPasswordDialog *dlg = new NewPasswordDialog();
+    dlg->exec();
+    delete dlg;
 }
